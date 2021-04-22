e9a5da7f5a545d1a90909afb359631a66a169991
sjk <tjswlsdl20@gmail.com>
2021-04-22 22:57:38 +0900

    aws config

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5d+3e4h1=1j1=le!7jfw+7&2n3p9b3p8gtoznf5x1r4p-x@le9'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'django2021-env.eba-wqv5mnps.us-west-2.elasticbeanstalk.com/',
+    'AWS-deploy-dev.us-west-2.elasticbeanstalk.com/',
 ]
 
 
